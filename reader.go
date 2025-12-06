858f0c08699b0aa5cafaad7926ef576f72dabd80
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2025-12-06 17:11:46 +0100

    Initial implementation

--- reader.go
+++ reader.go
@@ -1,60 +1,92 @@
 // Copyright 2025 Bjørn Erik Pedersen
 // SPDX-License-Identifier: MIT
 
-package golibtemplate
+package textandbinaryreader
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/binary"
 	"io"
 )
 
 // BlobMarker is used to identify start of a binary blob.
-var BlobMarker [8]byte = [8]byte{'T', 'A', '5', 'B', 'L', 'O', 'B', '1'}
+var BlobMarker [8]byte = [8]byte{'T', 'A', 'K', '3', '5', 'E', 'M', '1'}
 
-// Chunk holds a chunk of data.
-type Chunk struct {
-	ID   uint32
-	Body io.Reader
+// New creates a new Reader that reads from r and calls handleBlob
+// when a binary blob is encountered. Note that the io.Reader passed to handleBlob
+// is only valid during the call and any non-consumed data will be discarded.
+// The returned Reader represents the text stream with blobs filtered out.
+func New(r io.Reader, handleBlob func(id uint32, r io.Reader) error) io.Reader {
+	return &Reader{
+		r:          bufio.NewReader(r),
+		handleBlob: handleBlob,
+	}
 }
 
-func New() io.ReadCloser {
-	return &Reader{}
-}
+var _ io.Reader = (*Reader)(nil)
 
-var _ io.ReadCloser = (*Reader)(nil)
-
+// Reader is a reader that filters out binary blobs from a stream.
 type Reader struct {
-	textr    io.Reader
-	binaryr  io.Reader
-	currentr io.Reader // current reader: text or binary
-
-	buffIdx int      // current index in buff
-	buff    [12]byte // big enough for blob marker.
+	// underlying reader
+	r *bufio.Reader
+	// handleBlob is called when a blob is encountered.
+	// The reader passed to it receives the blob. Any non-consumed data will be discarded.
+	handleBlob func(id uint32, r io.Reader) error
 }
 
+// Read implements io.Reader.
 func (r *Reader) Read(p []byte) (n int, err error) {
-	// Read into buff until it is full, then decide what to do.
+	for n < len(p) {
+		peeked, err := r.r.Peek(len(BlobMarker))
+		if err != nil {
+			// Not enough bytes to form a marker, so the rest is text.
+			read, readErr := r.r.Read(p[n:])
+			n += read
+			return n, readErr
+		}
 
-	// Check if we have detected a blob marker.
+		if bytes.Equal(peeked, BlobMarker[:]) {
+			// Discard the marker.
+			_, err = r.r.Discard(len(BlobMarker))
+			if err != nil {
+				return n, err
+			}
 
-	// If we have detected a blob marker, write up to the marker to the current reader.
+			// Read the blob header.
+			id, size, err := ReadBlobHeaderExcludingMarker(r.r)
+			if err != nil {
+				return n, err
+			}
 
-	// Switch to binary reader.
+			// Create a limited reader for the blob and handle it.
+			lr := io.LimitReader(r.r, int64(size))
+			if err := r.handleBlob(id, lr); err != nil {
+				return n, err
+			}
 
-	// Read binary blob.
+			// Ensure the entire blob is consumed.
+			if _, err := io.Copy(io.Discard, lr); err != nil {
+				return n, err
+			}
 
-	// Switch back to text reader.
+			continue
 
-	panic("not implemented")
+		}
+
+		// Not a marker, read one byte.
+		p[n], err = r.r.ReadByte()
+		if err != nil {
+			return n, err
+		}
+		n++
+	}
+
+	return n, nil
 }
 
-func (r *Reader) Close() error {
-	// Flush any remaining data.
-	// Close any underlying readers if needed.
-	panic("not implemented")
-}
-
-func BlobHeaderWrite(w io.Writer, id uint32, size uint64) error {
+// WriteBlobHeader writes a blob header to w with the given id and size using little-endian encoding.
+func WriteBlobHeader(w io.Writer, id, size uint32) error {
 	if err := binary.Write(w, binary.LittleEndian, BlobMarker); err != nil {
 		return err
 	}
@@ -65,4 +97,27 @@ func BlobHeaderWrite(w io.Writer, id uint32, size uint64) error {
 		return err
 	}
 	return nil
+}
+
+// ReadBlobHeader reads a blob header from r and returns the id and size using little-endian encoding.
+func ReadBlobHeader(r io.Reader) (id, size uint32, err error) {
+	var marker [8]byte
+	if _, err := io.ReadFull(r, marker[:]); err != nil {
+		return 0, 0, err
+	}
+	if marker != BlobMarker {
+		return 0, 0, io.ErrUnexpectedEOF
+	}
+	return ReadBlobHeaderExcludingMarker(r)
+}
+
+// ReadBlobHeaderExcludingMarker reads a blob header from r excluding the marker using little-endian encoding.
+func ReadBlobHeaderExcludingMarker(r io.Reader) (id, size uint32, err error) {
+	if err := binary.Read(r, binary.LittleEndian, &id); err != nil {
+		return 0, 0, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &size); err != nil {
+		return 0, 0, err
+	}
+	return
 }
--- reader_test.go
+++ reader_test.go
@@ -1,10 +1,12 @@
 // Copyright 2025 Bjørn Erik Pedersen
 // SPDX-License-Identifier: MIT
 
-package golibtemplate
+package textandbinaryreader
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -12,16 +14,291 @@ import (
 
 func TestReader(t *testing.T) {
 	c := qt.New(t)
-	rc := New()
-	c.Assert(rc, qt.Not(qt.IsNil))
-	c.Assert(rc.Close(), qt.IsNil)
+
+	const testJSONTemplate = `{"item":"Item %d","price":%d}`
+	json := func(i int) string {
+		return fmt.Sprintf(testJSONTemplate, i, i*10)
+	}
+
+	writeBlob := func(w io.Writer, id uint32, data []byte) {
+		c.Helper()
+		err := WriteBlobHeader(w, id, uint32(len(data)))
+		c.Assert(err, qt.IsNil)
+		_, err = w.Write(data)
+		c.Assert(err, qt.IsNil)
+	}
+
+	blob1 := []byte{0xDE, 0xAD, 0xBE, 0xEF}
+	blob2 := []byte{0xCA, 0xFE, 0xBA, 0xBE}
+
+	testCases := []struct {
+		name        string
+		buildInput  func(c *qt.C) io.Reader
+		readBufSize int
+		handler     string // default, error, partialRead
+		wantOutput  string
+		wantBlobIDs []uint32
+		wantBlobs   [][]byte
+		wantErr     string
+	}{
+		{
+			name: "text-blob-text",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 101, blob1)
+				buf.WriteString(json(2))
+				return &buf
+			},
+			wantOutput:  json(1) + json(2),
+			wantBlobIDs: []uint32{101},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "starts with blob",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				writeBlob(&buf, 102, blob1)
+				buf.WriteString(json(1))
+				return &buf
+			},
+			wantOutput:  json(1),
+			wantBlobIDs: []uint32{102},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "ends with blob",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 103, blob1)
+				return &buf
+			},
+			wantOutput:  json(1),
+			wantBlobIDs: []uint32{103},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "multiple consecutive blobs",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				writeBlob(&buf, 104, blob1)
+				writeBlob(&buf, 201, blob2)
+				return &buf
+			},
+			wantOutput:  "",
+			wantBlobIDs: []uint32{104, 201},
+			wantBlobs:   [][]byte{blob1, blob2},
+		},
+		{
+			name: "multiple blobs not in sequence",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 105, blob1)
+				buf.WriteString(json(2))
+				writeBlob(&buf, 202, blob2)
+				buf.WriteString(json(3))
+				return &buf
+			},
+			wantOutput:  json(1) + json(2) + json(3),
+			wantBlobIDs: []uint32{105, 202},
+			wantBlobs:   [][]byte{blob1, blob2},
+		},
+		{
+			name: "zero-length blob",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 106, []byte{})
+				buf.WriteString(json(2))
+				return &buf
+			},
+			wantOutput:  json(1) + json(2),
+			wantBlobIDs: []uint32{106},
+			wantBlobs:   [][]byte{{}},
+		},
+		{
+			name: "partial marker in text",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				buf.Write(BlobMarker[:4])
+				buf.WriteString(json(2))
+				writeBlob(&buf, 107, blob1)
+				return &buf
+			},
+			wantOutput:  json(1) + string(BlobMarker[:4]) + json(2),
+			wantBlobIDs: []uint32{107},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "empty stream",
+			buildInput: func(c *qt.C) io.Reader {
+				return &bytes.Buffer{}
+			},
+			wantOutput: "",
+		},
+		{
+			name: "text only",
+			buildInput: func(c *qt.C) io.Reader {
+				return bytes.NewBufferString(json(1) + json(2))
+			},
+			wantOutput: json(1) + json(2),
+		},
+		{
+			name: "blob only",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				writeBlob(&buf, 108, blob1)
+				return &buf
+			},
+			wantOutput:  "",
+			wantBlobIDs: []uint32{108},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "small read buffer",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 109, blob1)
+				buf.WriteString(json(2))
+				return &buf
+			},
+			readBufSize: 5,
+			wantOutput:  json(1) + json(2),
+			wantBlobIDs: []uint32{109},
+			wantBlobs:   [][]byte{blob1},
+		},
+		{
+			name: "blob handler error",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 110, blob1)
+				return &buf
+			},
+			handler:    "error",
+			wantOutput: json(1),
+			wantErr:    "handler error",
+		},
+		{
+			name: "blob not fully read",
+			buildInput: func(c *qt.C) io.Reader {
+				var buf bytes.Buffer
+				buf.WriteString(json(1))
+				writeBlob(&buf, 111, blob1)
+				buf.WriteString(json(2))
+				return &buf
+			},
+			handler:     "partialRead",
+			wantOutput:  json(1) + json(2),
+			wantBlobIDs: []uint32{111},
+			wantBlobs:   [][]byte{blob1[:1]},
+		},
+	}
+
+	for _, tc := range testCases {
+		c.Run(tc.name, func(c *qt.C) {
+			var (
+				handledBlobs   [][]byte
+				handledBlobIDs []uint32
+			)
+
+			var handleBlob func(id uint32, r io.Reader) error
+			switch tc.handler {
+			case "error":
+				handleBlob = func(id uint32, r io.Reader) error {
+					return fmt.Errorf("handler error")
+				}
+			case "partialRead":
+				handleBlob = func(id uint32, r io.Reader) error {
+					handledBlobIDs = append(handledBlobIDs, id)
+					// Read only one byte.
+					b := make([]byte, 1)
+					_, err := r.Read(b)
+					handledBlobs = append(handledBlobs, b)
+					return err
+				}
+			default:
+				handleBlob = func(id uint32, r io.Reader) error {
+					b, err := io.ReadAll(r)
+					c.Assert(err, qt.IsNil)
+					handledBlobIDs = append(handledBlobIDs, id)
+					handledBlobs = append(handledBlobs, b)
+					return nil
+				}
+			}
+
+			r := New(tc.buildInput(c), handleBlob)
+			var outBuf bytes.Buffer
+			var err error
+
+			if tc.readBufSize > 0 {
+				_, err = io.CopyBuffer(&outBuf, r, make([]byte, tc.readBufSize))
+			} else {
+				_, err = io.Copy(&outBuf, r)
+			}
+
+			if tc.wantErr != "" {
+				c.Assert(err, qt.ErrorMatches, tc.wantErr)
+			} else {
+				c.Assert(err, qt.IsNil)
+			}
+
+			c.Assert(outBuf.String(), qt.Equals, tc.wantOutput)
+
+			if tc.wantBlobs != nil {
+				c.Assert(handledBlobs, qt.DeepEquals, tc.wantBlobs)
+			}
+			if tc.wantBlobIDs != nil {
+				c.Assert(handledBlobIDs, qt.DeepEquals, tc.wantBlobIDs)
+			}
+		})
+	}
 }
 
-func TestBlobHeaderWrite(t *testing.T) {
+func TestBlobHeaderWriteAndRead(t *testing.T) {
 	c := qt.New(t)
 
 	var b bytes.Buffer
-	err := BlobHeaderWrite(&b, 42, 100)
+	err := WriteBlobHeader(&b, 42, 100)
 	c.Assert(err, qt.IsNil)
 	c.Assert(b.Len(), qt.Equals, 8+4+8) // marker + id + size
+
+	id, size, err := ReadBlobHeader(&b)
+	c.Assert(err, qt.IsNil)
+	c.Assert(id, qt.Equals, uint32(42))
+	c.Assert(size, qt.Equals, uint64(100))
+}
+
+func BenchmarkReader(b *testing.B) {
+	bh := func(id uint32, r io.Reader) error {
+		// Do nothing.
+		return nil
+	}
+
+	var buf bytes.Buffer
+	for i := 0; i < 1000; i++ {
+		buf.WriteString(fmt.Sprintf(`{"item":"Item %d","price":%d}`, i, i*10))
+
+		// Write a blob.
+		blobData := []byte{0xDE, 0xAD, 0xBE, 0xEF}
+		err := WriteBlobHeader(&buf, uint32(i), uint32(len(blobData)))
+		if err != nil {
+			b.Fatal(err)
+		}
+		buf.Write(blobData)
+	}
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		r := New(&buf, bh)
+		_, err := io.Copy(io.Discard, r)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }
